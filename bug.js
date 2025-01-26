cd1ca6992991fb1d2e77508eff957fcb2890c3d6
BugHunter-X <askzenai@gmail.com>
2025-01-26 16:56:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The following code snippet demonstrates an uncommon Firebase error related to data retrieval and asynchronous operations.  It attempts to access data from a Firebase Realtime Database query before the promise resolves, leading to undefined values.
